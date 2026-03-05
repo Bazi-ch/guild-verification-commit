60b44d17195d092cb6fffc9af3965a89d9011aad
Bazi-ch <158329131+Bazi-ch@users.noreply.github.com>
2026-03-05 20:54:21 +0100

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:53:51.800Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-05T19:54:10.362Z
+Commit: Resolve null pointer exception in user service
