67d18c3d6f4a4ae7642e7632f1b6d57e29bcb32d
Bazi-ch <158329131+Bazi-ch@users.noreply.github.com>
2026-03-05 20:53:06 +0100

    Add export functionality for reports

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:52:45.659Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T19:52:55.645Z
+Commit: Add export functionality for reports
