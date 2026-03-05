4abec2dfe66b5562cfa520aeca172c8cb5bcc9a7
Bazi-ch <158329131+Bazi-ch@users.noreply.github.com>
2026-03-05 20:54:04 +0100

    Introduce real-time notifications

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:53:34.018Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T19:53:52.952Z
+Commit: Introduce real-time notifications
