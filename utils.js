02ed98a1277fc099b95c2bff2ccf61ea2680d140
Bazi-ch <158329131+Bazi-ch@users.noreply.github.com>
2026-03-05 20:55:02 +0100

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:54:46.085Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-05T19:54:50.882Z
+Commit: Fix race condition in async operations
