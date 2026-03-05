0fff00c88f9129a40cd3b813eb68ed3b405b6b20
Bazi-ch <158329131+Bazi-ch@users.noreply.github.com>
2026-03-05 20:53:10 +0100

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:52:58.363Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-05T19:52:59.568Z
+Commit: Patch API endpoint error handling
