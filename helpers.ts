1eef17ad1caddde9be29a4cd7fa4276edfce6297
Bazi-ch <158329131+Bazi-ch@users.noreply.github.com>
2026-03-05 20:53:30 +0100

    Implement user profile customization

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:53:04.363Z
-Commit: Fix database connection timeout
+Timestamp: 2026-03-05T19:53:19.473Z
+Commit: Implement user profile customization
