f460365485cfe50d51f63fb99fa1caeb7cd9f8a5
Bazi-ch <158329131+Bazi-ch@users.noreply.github.com>
2026-03-05 20:54:57 +0100

    Patch security vulnerability in authentication

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:54:20.870Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T19:54:46.085Z
+Commit: Patch security vulnerability in authentication
