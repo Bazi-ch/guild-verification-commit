a50bff7a8fd760d9db8990f61ce93cbb65ced8c2
Bazi-ch <158329131+Bazi-ch@users.noreply.github.com>
2026-03-05 20:53:04 +0100

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:52:42.609Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T19:52:52.787Z
+Commit: Fix CSS styling bug on mobile devices
