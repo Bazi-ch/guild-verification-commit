33743a71f646bd4cd42c3cef671bce744f4250d4
Bazi-ch <158329131+Bazi-ch@users.noreply.github.com>
2026-03-05 20:54:26 +0100

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:54:02.321Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T19:54:15.434Z
+Commit: Fix race condition in async operations
