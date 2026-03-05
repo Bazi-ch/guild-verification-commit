8774ffaed176c4f0bef77c70a9acd9c2739e84f4
Bazi-ch <158329131+Bazi-ch@users.noreply.github.com>
2026-03-05 20:53:42 +0100

    Introduce real-time notifications

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:53:24.056Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T19:53:31.660Z
+Commit: Introduce real-time notifications
