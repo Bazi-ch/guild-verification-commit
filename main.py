d9be18c79c18553a74864a92ad891fef010b5813
Bazi-ch <158329131+Bazi-ch@users.noreply.github.com>
2026-03-05 20:53:45 +0100

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:53:31.660Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T19:53:34.018Z
+Commit: Fix CSS styling bug on mobile devices
